776c39c9b62a9c36c41c4eae08b599fcf92b8592
Babur Makhmudov <bmuddha13@gmail.com>
2024-03-27 13:12:09 +0700

    fix: serde naming for transaction mode

--- src/types.rs
+++ src/types.rs
@@ -170,6 +170,7 @@ pub struct PerpPositionExtended {
 }
 
 #[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
 pub struct ModifyOrdersRequest {
     pub orders: Vec<ModifyOrder>,
     #[serde(default)]
@@ -230,6 +231,7 @@ impl ModifyOrder {
 }
 
 #[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
 pub struct PlaceOrdersRequest {
     pub orders: Vec<PlaceOrder>,
     #[serde(default)]
@@ -513,6 +515,7 @@ impl TxEventsResponse {
 }
 
 #[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct CancelAndPlaceRequest {
     pub cancel: CancelOrdersRequest,
     pub modify: ModifyOrdersRequest,
